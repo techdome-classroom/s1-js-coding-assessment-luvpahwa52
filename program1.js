99de8ab73c2325bc13d5b70d7d1faaef9533d5d9
luvpahwa52 <luvpahwa52@gmail.com>
2024-10-07 12:15:45 +0530

    program1 and program2

--- program1.js
+++ program1.js
@@ -1,8 +1,34 @@
-const getTotalIsles = function (grid) {
+const getTotalIsles = function(grid) {
+  if (!grid || grid.length === 0 || grid[0].length === 0) {
+      return 0;
+  }
 
+  const rows = grid.length;
+  const cols = grid[0].length;
+  let isles = 0;
 
-  // write your code here
+  const dfs = function(i, j) {
+      if (i < 0 || i >= rows || j < 0 || j >= cols || grid[i][j] !== 'L') {
+          return;
+      }
+      grid[i][j] = 'V';
 
+      dfs(i + 1, j);  
+      dfs(i - 1, j);  
+      dfs(i, j + 1);  
+      dfs(i, j - 1);  
+  };
+
+  for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < cols; j++) {
+          if (grid[i][j] === 'L') {
+              dfs(i, j);
+              isles++;
+          }
+      }
+  }
+
+  return isles;
 };
 
 module.exports = getTotalIsles;
